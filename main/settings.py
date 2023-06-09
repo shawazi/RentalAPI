a437677d3ecfbc97fd7b48fbeb98f9ffc553eee5
“shawazi” <shawazisonfire@gmail.com>
2023-06-09 05:35:46 -0400

    models instantiated

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sl$vxabxi%q(kam1hr%d)exmc)28_)%^ct2wsxf4o$sv76k5xc'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # My apps
+    
+    'rental_API',
+    
+    # 3rd party apps
+    
 ]
 
 MIDDLEWARE = [
--- rental_app/models.py
+++ rental_app/models.py
@@ -1,5 +1,30 @@
 from django.db import models
-from django.contrib.auth.mo
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Car(models.Model):
+    TRANSMISSION_OPTIONS = (
+        ("A", "Automatic"),
+        ("M", "Manual")
+    )
+    
+    plate_number = models.CharField(max_length=15)
+    make = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
+    year = models.IntegerField()
+    transmission = models.CharField(options=TRANSMISSION_OPTIONS, max_length=1)
+    rent_per_day = models.DecimalField(max_digits=10, decimal_places=2)
+    out_of_service = models.BooleanField()
+    
+    def __str__(self):
+        return f"{self.make} {self.model} {self.year} {self.plate_number} {self.transmission} {self.rent_per_day} {self.out_of_service}"
+    
+class Reservation(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    car = models.ForeignKey(Car, on_delete=models.PROTECT)
+    customer = models.ForeignKey(User, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return f"{self.customer} {self.car} {self.start_date} {self.end_date}"
